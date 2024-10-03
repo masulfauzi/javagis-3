828eb6de97daf315f949a2144b360b13fa2ff18b
masulfauzi <masul.fauzi@gmail.com>
2024-10-03 17:22:22 +0700

    fix: pindah easyprint ke lokal

--- app/Modules/Survey/Views/survey_export.blade.php
+++ app/Modules/Survey/Views/survey_export.blade.php
@@ -121,7 +121,7 @@
     <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
         integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo=" crossorigin=""></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/leaflet.draw/0.4.2/leaflet.draw.js"></script>
-    <script src="http://rowanwins.github.io/leaflet-easyPrint/dist/bundle.js"></script>
+    <script src="{{ asset('assets/js/extensions/easyprint/dist/bundle.js') }}"></script>
 
     <script>
         // OSM layers
