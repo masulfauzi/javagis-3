c5daf7ca43798223082284c4adcd5d33052b8915
masulfauzi <masul.fauzi@gmail.com>
2024-12-08 07:35:16 +0700

    tes lagi

--- app/Modules/Survey/Views/survey_polyline_tracking.blade.php
+++ app/Modules/Survey/Views/survey_polyline_tracking.blade.php
@@ -304,7 +304,7 @@
                     }
                 };
 
-                L.geoJSON(layerKps).addTo(map);
+                L.geoJSON(layerKps, {style: myStyle}).addTo(map);
                 <?php
                 }
                 ?>
